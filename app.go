248a44d28a55385ce17f463f45a651404e7b5ac8
Sun <24259556+sunx2@users.noreply.github.com>
2019-08-14 00:20:49 +0530

    updated first go code

--- app.go
+++ app.go
@@ -1,4 +1,15 @@
 package main
-func main() {
 
+import "fmt"
+
+func main() {
+	fmt.Print(Reverse("hello world"))
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
